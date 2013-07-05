202ba77613d205413ea57138eecdf928dafdf90d
Neal Blackburn <neblackburn@gmail.com>
2013-07-05 15:55:53 -0700

    Update setup-lamp.sh
    Updated to clone the screenrc and vimrc repos, symlink them and create a ~/tmp folder for vim swap files

--- setup-lamp.sh
+++ setup-lamp.sh
@@ -1,10 +1,18 @@
 yum -y update
+yum install -y screen
 yum install -y mysql mysql-server
 chkconfig --levels 235 mysqld on
 /etc/init.d/mysqld start
-mysql_secure_installation 
+mysql_secure_installation
 yum install -y httpd
 chkconfig --levels 235 httpd on
 /etc/init.d/httpd start
 yum install -y php php-mysql php-gd php-mbstring php-pear php-pecl-apc
-service iptables restart
+
+mkdir ~/tmp
+
+git clone https://github.com/neblackburn/vimrc.git
+ln -s vimrc/.vimrc ~/.vimrc
+
+git clone https://github.com/neblackburn/screenrc.git
+ln -s screenrc/.screenrc ~/.screenrc
